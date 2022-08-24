71f0567879973a9b25d501b90381dc7742571cc3
kumasuke6 <kb_kumagki@yahoo.co.jp>
2022-08-24 21:35:04 +0900

    top, ptページのlayoutの共通化

---
+++ src/resources/views/layouts/header.blade.php
@@ -0,0 +1,25 @@
+@section('header')
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">過去問勉強帳</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#Navbar" aria-controls="Navbar" aria-expanded="false" aria-label="ナビゲーションの切替">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="Navbar">
+          <ul class="navbar-nav me-auto mb-2 mb-md-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">ホーム</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">リンク</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled">無効</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+</header>
+@endsection
