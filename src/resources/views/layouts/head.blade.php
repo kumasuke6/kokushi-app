f7fd11abc6a070f8598907a55a2ccc7ff040a9b4
kumasuke6 <kb_kumagki@yahoo.co.jp>
2023-01-11 22:07:31 +0900

    #13 修正

--- src/resources/views/layouts/head.blade.php
+++ src/resources/views/layouts/head.blade.php
@@ -3,9 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>セラピスト国試エキスパート</title>
-    {{-- TODO:CDNではなくインストールして使用する。 --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js"></script>
     @yield('pageCss')
 @endsection
